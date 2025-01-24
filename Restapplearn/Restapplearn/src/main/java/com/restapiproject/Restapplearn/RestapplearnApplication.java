19479bcf217d8599deb5e86dd79d7040a74b18b8
Ayandah-D <87431114+Ayandah-D@users.noreply.github.com>
2025-01-24 11:32:07 +0200

    API Project

---
+++ Restapplearn/Restapplearn/src/main/java/com/restapiproject/Restapplearn/RestapplearnApplication.java
@@ -0,0 +1,13 @@
+package com.restapiproject.Restapplearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapplearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapplearnApplication.class, args);
+	}
+
+}
---
+++ Restapplearn/Restapplearn/src/test/java/com/restapiproject/Restapplearn/RestapplearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapiproject.Restapplearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapplearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
